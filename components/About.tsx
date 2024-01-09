8404a9c929e1463b7bdffc6730ce1710db681f91
Himanshu Ranjan <rhimanshu@vmware.com>
2024-01-09 22:39:15 +0530

    chore: update description

--- components/About.tsx
+++ components/About.tsx
@@ -44,8 +44,7 @@ function About({}: Props) {
         implementation, and testing of new features. My toolbox includes working in a 
         micro service environment using Golang, PostgreSQL, Docker, and other technologies 
         that allow me to bring ideas to life. I find great joy in collaborating with internal 
-        teams and vendors to enhance and implement new features. 
-        </ br>
+        teams and vendors to enhance and implement new features.<br>
         Let&apos;s connect and explore how we can create something remarkable together or simply 
         chat about the fascinating world of software development.
         </p>
